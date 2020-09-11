ec3f0f37304cfd722cac244b863d0521f5c8b35c
Arpit Gupta <imarg_3@zoho.com>
2020-09-12 00:17:27 +0530

    Add code for Time Complexity

--- src/code/TimeComplexityMain.java
+++ src/code/TimeComplexityMain.java
@@ -5,22 +5,50 @@ public class TimeComplexityMain {
 	public static void main(String[] args) {
 		
 		int result;
-		result = TimeComplexity.factorial(5);
+		result = TimeComplexityMain.factorial(5);
 		System.out.println("5! = " + result);
 		System.out.println("----------------------");
 		
-		TimeComplexity.prime(100);
+		TimeComplexityMain.prime(100);
 		System.out.println("----------------------");
 		
-		TimeComplexity.decimalToBinary(10);
+		TimeComplexityMain.decimalToBinary(10);
 		System.out.println("----------------------");
 		
-		TimeComplexity.printTable(28);
+		TimeComplexityMain.printTable(28);
 		System.out.println("----------------------");
-		
-		int numbers[] = {21, 53, 45, 98, 35, 14, 67, 54, 32, 16};
-		int valueToSearch = 45;
-		int indexOfValue = TimeComplexity.linearSearch(numbers, valueToSearch);
-		System.out.println(indexOfValue);
+	}
+	
+	public static int factorial(int n) {
+		int res = 1;
+		for(int i=1; i<=n; i++)
+			res = res * i;
+		return res;
+	}
+	
+	public static void prime(int n) {
+		for (int num = 0; num <= n; num++) {
+			int i;
+			for (i = 2; i < num; i++) {
+				if(num % i == 0)
+					break;
+			}
+			if(i == num)
+				System.out.println(num);
+		}
+	}
+	
+	public static void printTable(int n) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(n * i);
+		}
+	}
+	
+	public static void decimalToBinary(int num) {
+		while(num > 0) {
+			System.out.print(num % 2);
+			num = num / 2;
+		}
+		System.out.println();
 	}
 }
