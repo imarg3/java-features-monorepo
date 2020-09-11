fe6d0d642d60d25126a5e220fd43474b20b50896
Arpit Gupta <imarg_3@zoho.com>
2020-09-12 00:14:56 +0530

    Implement Selection & Bubble Sort

--- src/code/SortingMain.java
+++ src/code/SortingMain.java
@@ -4,14 +4,24 @@ public class SortingMain {
 	public static void main(String[] args) {
 		int[] arr = {5, 4, 2, 6, 3, 1};
 		selectionSort(arr);
+		System.out.println("Selection Sort :: Ascending Output :");
 		for (int i = 0; i < arr.length; i++)
 			System.out.println(arr[i]);
 		
-		System.out.println("----------------");
+		System.out.println("");
 		
 		selectionSortDescending(arr);
+		System.out.println("Selection Sort :: Descending Output :");
 		for (int i = 0; i < arr.length; i++)
 			System.out.println(arr[i]);
+		
+		System.out.println("");
+		
+		bubbleSort(arr);
+		System.out.println("Bubble Sort :: Ascending Output :");
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
+
 	}
 
 	// ascending
@@ -41,6 +51,21 @@ public class SortingMain {
 					int temp = arr[i];
 					arr[i] = arr[j];
 					arr[j] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void bubbleSort(int[] arr) {
+		// run n-1 passes for bubble sort
+		for (int i = 0; i < arr.length-1; i++) {
+			// compare all consecutive elements of the array
+			for (int j = 0; j < arr.length-1; j++) {
+				// if j th element > j+1 th element, swap them
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
 				}
 			}
 		}
